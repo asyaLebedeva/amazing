ca0a51b40af87e1309e2d4b5e36885b3fb24a44a
Asya <asya.lebedeva742@gmail.com>
2023-10-05 14:34:52 -0700

    created gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+const { src, dest, series, watch } = require('gulp')
+const htmlminify = require('gulp-htmlmin')
+const cssminify = require('gulp-clean-css')
+const autoprefix = require('gulp-autoprefixer')
+const styleConcat = require('gulp-concat-css')
+const JSconcat = require('gulp-concat')
+const svg = require('gulp-svgo')
+const browSync = require('browser-sync').create()
+
+
+const browser = () => {
+  browSync.init({
+    server: {baseDir: 'dist'}
+  })
+}
+
+const htmlmin = () => {
+  return src('src/*.html')
+  .pipe(htmlminify({
+    collapseWhitespace: true,
+    useShortDoctype: true,
+    removeComments: true
+  }))
+  .pipe(dest('dist'))
+  .pipe(browSync.stream())
+}
+
+const styles = () => {
+  return src('src/css/**/*.css')
+  .pipe(autoprefix({
+    cascade: false
+  }))
+  .pipe(styleConcat('style.css'))
+  .pipe(cssminify({
+    level: 2,
+    compatibility: 'ie9'
+  }))
+  .pipe(dest('dist'))
+  .pipe(browSync.stream())
+}
+
+const scripts = () => {
+  return src('src/js/*.js')
+  .pipe(JSconcat('all.js'))
+  .pipe(dest('./dist/'))
+  .pipe(browSync.stream())
+}
+
+const img = () => {
+  return src('src/img/**/*.svg')
+  .pipe(svg())
+  .pipe(dest('./dist/img/svg'))
+}
+
+
+exports.scripts = scripts
+exports.htmlmin = htmlmin
+exports.styles = styles
+exports.img = img
+exports.default = series(htmlmin, scripts, styles, browser)
