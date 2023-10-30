d8e59b7c6df77f5e583d4d3f6542c0b6a41bc74f
Asya <asya.lebedeva742@gmail.com>
2023-10-30 05:56:56 -0700

    add new scripts for media-queries

--- src/js/script.js
+++ src/js/script.js
@@ -3,16 +3,15 @@ $(document).ready(function() {
     $('.menu-trigger, .enter-aside').css('display', 'none');
   }).on('mouseleave', function() {
     $('.menu-trigger').css('display', 'block');
-    //$('.enter-aside').css('display', 'flex');
   });
 });
 
 function checkMediaQuery() {
-  if (window.matchMedia("(max-width: 1825px)").matches) {
-    let scroll_pos = 0;
+  let scroll_pos = 0;
+  if (window.matchMedia("(max-width: 1400px) and (max-width: 1825px)").matches) {
     $(document).scroll(function() { 
       scroll_pos = $(this).scrollTop();
-      if (scroll_pos > 1400) {
+      if (scroll_pos > 1300) {
         $('.menu-trigger, .enter-aside').css('background-color', 'rgba(11, 29, 38, 0.7)');
         $('.enter-aside').css('border-radius', '8px');
         $('.menu-trigger').css({'border-top-left-radius': '8px', 'border-bottom-left-radius' : '8px'});
@@ -32,17 +31,23 @@ function checkMediaQuery() {
       });
     });
   }
+
+  if(window.matchMedia("(max-width: 575px)").matches) {
+    $(document).scroll(function() { 
+      scroll_pos = $(this).scrollTop();
+      if (scroll_pos > 600) {
+        $('.menu-trigger, .enter-aside').css('background-color', 'rgba(11, 29, 38, 0.7)');
+        $('.enter-aside').css('border-radius', '5px');
+        $('.menu-trigger').css({'border-top-left-radius': '5px', 'border-bottom-left-radius' : '5px'});
+      } else {
+        $('.menu-trigger, .enter-aside').css('background-color', 'transparent');
+        $('.menu-trigger, .enter-aside').css('border-radius', '0px');
+      }
+    });
+  }
 }
 checkMediaQuery();
 $(window).on('resize', checkMediaQuery);
-/*function checkMediaQuery() {
-  const large = window.matchMedia('(max-width: 1825px)')
-  const middle = window.matchMedia('(max-width: 1400px)')
-  const small = window.matchMedia('(max-width: 575px)')
-  large.addEventListener(function(e) {
-    
-  })
-}*/
 
 let sections = $('section')
   , nav = $('nav')
